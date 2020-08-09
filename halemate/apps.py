52f9cfd57e86892c9f873593c9f4e17d6957cbd0
Mrinal <kmrinal19@gmail.com>
2020-08-09 20:50:48 +0530

    conf: initialized django project

---
+++ halemate/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HalemateConfig(AppConfig):
+    name = 'halemate'
---
+++ halemate/models.py
@@ -0,0 +1,61 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+
+    # choice to register as user or hospital
+    USER = 'U'
+    HOSPITAL = 'H'
+    register_choices = [(USER, 'User'), (HOSPITAL, 'Hospital')]
+
+    first_name = None
+    last_name = None
+    username = None
+    email = models.EmailField(unique = True)
+    name = models.CharField(max_length = 180)
+    phoneNumber = models.CharField(max_length = 15, unique = True)
+    medical_history = models.TextField(blank = True)
+    registered_as = models.CharField(max_length = 1, choices = register_choices, default = USER)
+    is_verified = models.BooleanField(default = False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
